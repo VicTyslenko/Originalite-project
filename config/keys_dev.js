ca76aca56b58986eda7337347fb863c60140e524
Victor <vidrums86@gmail.com>
2024-12-16 22:01:51 +0000

    upd: changed env file, moved data base to other account

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb+srv://admin:admin@onlinemagazine.3spckjc.mongodb.net/dataBD`,
-  secretOrKey: "random very very secret string",
+  mongoURI: process.env.mongoURI,
+  secretOrKey: "secret key for token",
 };
