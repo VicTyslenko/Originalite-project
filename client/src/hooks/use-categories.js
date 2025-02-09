457347b7f478226b0f84ca60d32c48e72d24e535
Victor <vidrums86@gmail.com>
2025-02-09 21:43:08 +0000

    fixing deployment

--- client/src/hooks/use-categories.js
+++ client/src/hooks/use-categories.js
@@ -3,8 +3,7 @@ import { useSelector } from "react-redux";
 export const useCategories = category => {
 	const allCategories = useSelector(state => state.categories.data);
 
-
-	const filteredCategories = allCategories.filter(item => item.parentId === category);
+	const filteredCategories = allCategories ? allCategories.filter(item => item.parentId === category) : [];
 
 	return { filteredCategories, allCategories };
 };
