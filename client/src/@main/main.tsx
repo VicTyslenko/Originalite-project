253add7c1570ecc30858412c84659b45cd8afa14
Victor <vidrums86@gmail.com>
2025-02-19 21:43:31 +0000

    upd: added linter for developing/unused imports

---
+++ client/src/@main/main.tsx
@@ -0,0 +1,30 @@
+import AddressDetails from "@profile/containers/AdressDetails/AddressDetails";
+import { useUserData } from "hooks/use-user-data";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import MainRoutes from "./router";
+import { getCart } from "./store/actions/cartActions";
+import { getWishlist } from "./store/actions/wishlistActions";
+
+function Main() {
+  const dispatch = useDispatch();
+
+  const user = useUserData();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getCart());
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (user) {
+      dispatch(getWishlist());
+    }
+  }, [user]);
+
+  return <MainRoutes />;
+}
+
+export default Main;
