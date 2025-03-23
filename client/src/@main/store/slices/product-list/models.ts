db287189edb3933c5c345eb80ae469e318f5461b
Victor <vidrums86@gmail.com>
2025-03-23 22:14:57 +0000

    fix: fixed types in ProductList

--- client/src/@main/store/slices/product-list/models.ts
+++ client/src/@main/store/slices/product-list/models.ts
@@ -1,6 +1,6 @@
 import type { ProductData } from "shared/models/products.models";
 
 export type ProductListProps = {
-  products: ProductData[];
+  products: ProductData[] | null;
   productsQuantity: number;
 };
