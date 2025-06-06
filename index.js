2cdf5309d52695afa0a041423757189de21a9f38
Victor <vidrums86@gmail.com>
2025-06-06 21:50:45 +0100

    testing...

--- index.js
+++ index.js
@@ -71,14 +71,6 @@ app.use("/api/slides", slides);
 app.use("/api/subscribers", subscribers);
 app.use("/api/wishlist", wishlist);
 
-// if (process.env.NODE_ENV == "production") {
-//   app.use(express.static(path.join(__dirname, "client", "build")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
 app.listen(4444, (err) => {
   if (err) {
     return console.log(err);
