feb755aa867163263f00474919b69d1187f33688
Victor <vidrums86@gmail.com>
2025-06-09 22:50:13 +0100

    testing...

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -1,10 +1,12 @@
-const whitelist = ["http://localhost:3000", "https://originalite-project.onrender.com"];
+const whitelist = ["http://localhost:3000", "https://originalite-shop.vercel.app"];
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
 
   if (whitelist.includes(origin)) {
     res.header("Access-Control-Allow-Credentials", true);
+  } else {
+    console.warn(`Origin not in whitelist: ${origin}`);
   }
 
   next();
